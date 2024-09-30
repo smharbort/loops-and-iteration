d81ac377af21cbc096996e508048ccf1c70ade28
smh <smharbort@outlook.com>
2024-09-30 13:45:10 -0400

    initializing repo and fizz buzz answer iteration 1

---
+++ loops-and-iteration.js
@@ -0,0 +1,51 @@
+/*
+
+Okay, so, there are a few different types of for & while loops.
+
+                                For loops:
+                for
+
+for (initialization; condition; afterthought) {
+    execute
+}
+
+                for...in
+
+for (const i in str) {
+    execute
+}
+
+                for...of
+
+for (const c of str) {
+    execute
+}
+
+                                While loops:
+                while
+
+while (condition)
+
+                do...while
+
+
+
+                labeled whiles
+
+
+
+*/
+
+//                                                              PART ONE
+// loop thru numbers 1 - 100
+for (let i = 1; i <= 100; i++) {
+    if ((i % 3 == 0) && (i % 5 != 0)) {
+        console.log("Fizz")
+    } else if ((i % 3 != 0) && (i % 5 == 0)) {
+        console.log("Buzz")
+    } else if ((i % 3 == 0) && (i % 5 == 0)) {
+        console.log("Fizz Buzz")
+    } else {
+        console.log(i)
+    }
+}
